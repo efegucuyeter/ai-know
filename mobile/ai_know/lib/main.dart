505d9f4611b3d40901f791912d1b7c6c608ec344
Talha Güleryüz <162495684+TalhaGuer@users.noreply.github.com>
2025-05-04 23:05:27 +0300

    Main Dart Güncellendi
    Main Dart Güncellendi

--- mobile/ai_know/lib/main.dart
+++ mobile/ai_know/lib/main.dart
@@ -1,30 +1,20 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
-import 'screens/registration_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/profile_screen.dart';
-import 'screens/search_screen.dart';
+import 'core/routes.dart';
+import 'core/themes.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Quiz APP',
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => LoginScreen(),
-        '/register': (context) => RegistrationScreen(),
-        '/home': (context) => HomeScreen(),
-        '/profile': (context) => ProfileScreen(),
-        '/search': (context) => SearchScreen(),
-      },
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      theme: AppThemes.lightTheme,
+      routerConfig: appRouter, // routes.dart'tan import edilen
     );
   }
 }
